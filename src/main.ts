ba95ac42b1f8a48199f429a3799e11220e1fe706
Camilo Salazar <csalazar@creditu.com>
2021-10-12 19:58:31 -0300

    Implementa CORS

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const env = process.env.NODE_ENV || 'local';
+const whitelist = [];
+if (env === 'prod') {
+  whitelist.push('https://creditu-players-assessment-ui.herokuapp.com');
+} else {
+  whitelist.push(/http:\/\/localhost:[0-9]+$/);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({ origin: whitelist });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
